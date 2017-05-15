4064c8e8c3bab871a793a5ff4f16489d58925ccd
Logan Bernard <bernard.logan4@gmail.com>
2017-05-15 13:57:10 -0400

    remove moisan-redux specific logic (mostly)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 
 import user from './user'
-import form from './form'
 
 const rootReducer = combineReducers({
   user,
-  form,
   routing: routerReducer
 })
 
